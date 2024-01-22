6b3eede2478151acf68e20ae85263c6666655ae3
Bolu <oo222515@gmail.com>
2024-01-21 23:22:37 -0600

    This is current final work

---
+++ Practice 2/script.js
@@ -0,0 +1,11 @@
+// function alertUser() {
+//     alert('Hey!! You clicked me!');
+// }
+
+const button = document.querySelector('#btn');
+// button.onclick = alertUser;
+// button.addEventListener('click', function() {
+//     alert('Hey!! You clicked me!');
+// })
+
+button.addEventListener('click', () => alert('Hey!! You clicked me!'));
